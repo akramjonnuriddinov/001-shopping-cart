fc77181de7e8e7bae78f2d9e1a0ff064c7d42ee6
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2022-07-05 21:07:11 +0500

    Fix bug in decr btn

--- js/main.js
+++ js/main.js
@@ -7,12 +7,8 @@ const elCartPrice = document.querySelectorAll('.js-cart-price');
 if (elIncrBtn) {
     elIncrBtn.forEach(function(item, index) {
         const myVar = elCartPrice[index].textContent;
-        // let count = elCartCount[index].textContent;
         elIncrBtn[index].addEventListener('click', function () {
             elCartCount[index].textContent++;
-            // count++;
-            // console.log(count);
-            // elCartPrice[index].textContent = count * myVar;
             elCartPrice[index].textContent = elCartCount[index].textContent * myVar;
         });
     });
@@ -21,15 +17,10 @@ if (elIncrBtn) {
 if (elDecrBtn) {
     elDecrBtn.forEach(function (item, index) {
         const myVar = elCartPrice[index].textContent;
-        // let count = elCartCount[index].textContent;
         elDecrBtn[index].addEventListener('click', function () {
             if (elCartCount[index].textContent > 0) {
                 elCartCount[index].textContent--;
-                // count --;
-                // console.log(count);
                 elCartPrice[index].textContent = elCartCount[index].textContent * myVar;
-                // console.log(elCartCount[index].textContent);
-                // console.log(myVar);
             }
         });
     });
